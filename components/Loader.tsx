b56e8f3a68add0f4c1dfb378d1c6aa162f3edd7f
xpertaha <matemailingagency@gmail.com>
2025-07-12 21:56:57 +0100

    Create Loader.tsx

---
+++ components/Loader.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Loader: React.FC = () => {
+    return (
+        <div className="flex justify-center items-center my-8">
+            <div className="w-12 h-12 border-4 border-gray-600 border-t-blue-500 rounded-full animate-spin"></div>
+        </div>
+    );
+};
+
+export default Loader;
