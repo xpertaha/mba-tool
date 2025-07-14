62556535bdcc344bde865bc10bd2cf277f5698f2
xpertaha <matemailingagency@gmail.com>
2025-07-14 15:57:02 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,22 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
 import App from './App';
-import i18n from './i18n';
+import i18n from './i18n'; // Import the configured i18n instance
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-    throw new Error("Could not find root element to mount to");
+  throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
-
-// We render the app only AFTER i18next has been initialized
-i18n.init().then(() => {
-    root.render(
-        <React.StrictMode>
-            <I18nextProvider i18n={i18n}>
-                <App />
-            </I18nextProvider>
-        </React.StrictMode>
-    );
-});
+root.render(
+  <React.StrictMode>
+    <React.Suspense fallback="Loading...">
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </React.Suspense>
+  </React.StrictMode>
+);
